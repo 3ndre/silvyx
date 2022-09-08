d56d4b210a41aa394ed62eedae92bbd0b3284cc7
3ndless_____ <57608074+3ndre@users.noreply.github.com>
2022-09-08 15:20:02 +0545

    Added send transaction function

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,38 @@
-require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    matic: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/ENZXGT0Qxaq7fqJLJcYoJRThBUY3yZnY', 
+      accounts: [""],
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
+
+
+
--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,3 +1,11 @@
+import * as React from 'react'
+import { useDebounce } from 'use-debounce'
+import {
+  usePrepareSendTransaction,
+  useSendTransaction,
+  useWaitForTransaction,
+} from 'wagmi'
+
 import { useAccount } from 'wagmi'
 import { Navigate } from 'react-router-dom';
 // @mui
@@ -12,7 +20,30 @@ import Page from '../components/Page';
 export default function Transaction() {
   const { themeStretch } = useSettings();
 
-  const { isConnected } = useAccount()
+  const ethers = require("ethers");
+
+  const { isConnected, address } = useAccount()
+
+  const [to, setTo] = React.useState('')
+  const [debouncedTo] = useDebounce(to, 500)
+
+  const [amount, setAmount] = React.useState('')
+  const [debouncedAmount] = useDebounce(amount, 500)
+
+  const { config } = usePrepareSendTransaction({
+    request: {
+      to: debouncedTo,
+      value: debouncedAmount ? ethers.utils.parseEther(debouncedAmount) : undefined,
+    },
+  })
+  const { data, sendTransaction } = useSendTransaction(config)
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+
+
 
 
   if (!isConnected) {
@@ -26,6 +57,37 @@ export default function Transaction() {
           Transaction
         </Typography>
 
+
+        <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        sendTransaction?.()
+      }}
+    >
+      <input
+        aria-label="Recipient"
+        onChange={(e) => setTo(e.target.value)}
+        placeholder="0xA0Cf…251e"
+        value={to}
+      />
+      <input
+        aria-label="Amount (ether)"
+        onChange={(e) => setAmount(e.target.value)}
+        placeholder="0.05"
+        value={amount}
+      />
+      <button disabled={isLoading || !sendTransaction || !to || !amount || to === address}>
+        {isLoading ? 'Sending...' : 'Send'}
+      </button>
+      {isSuccess && (
+        <div>
+          Successfully sent {amount} ether to {to}
+          <div>
+            <a href={`https://mumbai.polygonscan.com/tx/${data?.hash}`}>Polygonscan</a>
+          </div>
+        </div>
+      )}
+    </form>
         
         
       </Container>
